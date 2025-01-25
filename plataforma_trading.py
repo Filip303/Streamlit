c704b2102b25159cfe760d4d3f5228ba9e7ecaff
Nicolae Filip Stanciu <filipstanciu@outlook.es>
2025-01-25 17:08:23 +0100

    Update plataforma_trading.py

--- plataforma_trading.py
+++ plataforma_trading.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ZVyQcMpzYUXu67RpK_OBJwHKgRvA2GHg
 """
 
-pip install streamlit pandas yfinance plotly
 
 import streamlit as st
 import pandas as pd
